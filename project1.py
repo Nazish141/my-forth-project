5c9c59d778c9b984fe6c44b746f75b5d925ddcb7
Nazish <nazishnoreen01@email.com>
2025-07-07 13:14:39 +0500

    my forth project

---
+++ project1.py
@@ -0,0 +1,19 @@
+str1 = "My name is nazish.\tI am doing well."
+print(str1)
+str2 = "nazish noor"
+print(len(str2))
+#indexing
+str = "nazish noor"
+ch1= str[0]
+ch2 = str[1]
+ch3 = str[2]
+ch4 = str[3]
+ch5 = str[4]
+ch7 = str[6]
+print(ch1)
+print(ch2)
+print(ch3)
+print(ch4)
+print(ch5)
+print(ch7)  
+
